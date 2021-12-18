e4539171a231f8514e3eed0ec31fc77570eacd57
Naufal Aufa <rahatta27@gmail.com>
2021-12-18 16:58:58 +0700

    commit

--- algorithm.js
+++ algorithm.js
@@ -1,12 +1,16 @@
 function searchalgorithm(){
     var kata = document.getElementById("typed").value;
     document.getElementById("penjelasan").innerHTML = "Mencari arti dari <span>" + kata + "</span>";
+    
+    
     fetch("database.json")
         .then((response) => response.json())
         .then((data) => displayData(data));
+    
 }
 
 function displayData(d){
+    var database = JSON.parse(d);
     console.log("Hi!")
     document.getElementById("translate").innerHTML = d;
 }
