9e9630ae6552804f96bfc3129325468b86dad3fe
Naufal Aufa <rahatta27@gmail.com>
2021-12-20 09:36:58 +0700

    commit

--- algorithm.js
+++ algorithm.js
@@ -1,16 +1,33 @@
 function searchalgorithm(){
-    var kata = document.getElementById("typed").value;
-    document.getElementById("penjelasan").innerHTML = "Mencari arti dari <span>" + kata + "</span>";
-    
-    
-    fetch("database.json")
-        .then((response) => response.json())
-        .then((data) => displayData(data));
-    
-}
+    var k = (document.getElementById("typed").value).toLowerCase();
+    var kataIndo = k.split(" ");
+    var kataJawa = [];
+    var outputJawa = "";
 
-function displayData(data){
-    var d = JSON.parse(data);
-    console.log("Hi!")
-    document.getElementById("translate").innerHTML = d;
+    console.log(kataIndo);
+    document.getElementById("penjelasan").innerHTML = "Mencari arti dari <span>" + k + "</span>";
+    
+    // bikin database
+    for(var i = 0; i < kataIndo.length; i++){
+        switch(kataIndo[i]){
+            case "kamu":
+                kataJawa.push("sampeyan")
+                break;
+            case "saya" :
+                kataJawa.push("kula")
+                break;
+            default :
+                kataJawa.push(kataIndo[i])
+        }
+    }
+
+    console.log(outputJawa)
+
+    for(var i = 0; i < kataJawa.length; i++){
+        console.log(outputJawa)
+        outputJawa += kataJawa[i];
+        outputJawa += " ";
+    }
+    
+    document.getElementById("translate").innerHTML = outputJawa;
 }
